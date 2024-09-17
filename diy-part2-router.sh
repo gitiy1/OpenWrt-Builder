42805dd472336b8050df929b8bfcfa575d0c5dcf
Git IY! <91715524+gitiy1@users.noreply.github.com>
2024-09-18 02:34:30 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -87,11 +87,11 @@ config_package_add luci
 config_package_add default-settings-chn
 # bbr
 config_package_add kmod-tcp-bbr
-# coremark cpu 跑分
-config_package_add coremark
-# autocore + lm-sensors-detect： cpu 频率、温度
-config_package_add autocore
-config_package_add lm-sensors-detect
+# # coremark cpu 跑分
+# config_package_add coremark
+# # autocore + lm-sensors-detect： cpu 频率、温度
+# config_package_add autocore
+# config_package_add lm-sensors-detect
 # nano 替代 vim
 config_package_add nano
 # upnp
@@ -147,6 +147,9 @@ config_package_add bash
 config_package_add tcpdump
 #netcat
 config_package_add netcat
+#tailsacle
+config_package_add tailscale
+
 
 # 第三方软件包
 mkdir -p package/custom
