227a4842706375ccc158ecc69797f084040d0a85
Git IY! <91715524+gitiy1@users.noreply.github.com>
2024-09-18 03:10:44 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -92,7 +92,7 @@ config_package_add coremark
 # autocore + lm-sensors-detect： cpu 频率、温度
 config_package_add autocore
 config_package_add lm-sensors-detect
-nano 替代 vim
+#nano 替代 vim
 config_package_add nano
 # upnp
 config_package_add luci-app-upnp
