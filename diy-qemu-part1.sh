d0096ab7e62b2b9b3b759c4811a86718e5be8005
Git IY! <91715524+gitiy1@users.noreply.github.com>
2024-09-18 13:52:50 +0800

    Update diy-qemu-part1.sh

--- diy-qemu-part1.sh
+++ diy-qemu-part1.sh
@@ -19,4 +19,4 @@ sed -i 's,noinitrd,noinitrd mitigations=off,g' target/linux/x86/image/grub-iso.c
 sed -i 's,noinitrd,noinitrd mitigations=off,g' target/linux/x86/image/grub-pc.cfg
 
 # 使用 Linux 6.6 内核
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=68./g' target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=68./g' target/linux/x86/Makefile
