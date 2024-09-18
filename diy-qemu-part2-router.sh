cbbac38045732aa711041210337a92e6d989ac26
Git IY! <91715524+gitiy1@users.noreply.github.com>
2024-09-18 13:58:10 +0800

    Update diy-qemu-part2-router.sh

--- diy-qemu-part2-router.sh
+++ diy-qemu-part2-router.sh
@@ -124,7 +124,7 @@ config_package_add kmod-usb-storage
 config_package_add kmod-macvlan
 config_package_add mwan3
 config_package_add luci-app-mwan3
-config_package_add luci-app-syncdial
+# config_package_add luci-app-syncdial
 #qemu guest agent
 config_package_add qemu-ga
 #umdns
@@ -181,14 +181,14 @@ config_package_add luci-lib-ipkg
 config_package_add luci-app-partexp
 ## iStore 应用市场
 config_package_add luci-app-store
-## 4G/5G 支持：FM350-GL USB RNDIS
-### Siriling/5G-Modem-Support
-config_package_add luci-app-modem
-config_package_add luci-app-sms-tool-js
-### luci-app-modemband
-config_package_add luci-app-modemband
-### luci-app-3ginfo-lite
-config_package_add luci-app-3ginfo-lite
+# ## 4G/5G 支持：FM350-GL USB RNDIS
+# ### Siriling/5G-Modem-Support
+# config_package_add luci-app-modem
+# config_package_add luci-app-sms-tool-js
+# ### luci-app-modemband
+# config_package_add luci-app-modemband
+# ### luci-app-3ginfo-lite
+# config_package_add luci-app-3ginfo-lite
 
 # 镜像生成
 # 修改分区大小
