f56b062973151c78ac1d33e299464bdd95e02480
Git IY! <91715524+gitiy1@users.noreply.github.com>
2024-09-19 18:40:57 +0800

    Update diy-qemu-part2-router.sh

--- diy-qemu-part2-router.sh
+++ diy-qemu-part2-router.sh
@@ -205,7 +205,7 @@ config_del GRUB_IMAGES
 # 生成 QCOW2 镜像
 config_add QCOW2_IMAGES
 
-config_add CONFIG_ACPI
-config_add CONFIG_EFI
-config_add CONFIG_EFI_VARS
-config_add CONFIG_EFI_STUB
+# config_add CONFIG_ACPI
+# config_add CONFIG_EFI
+# config_add CONFIG_EFI_VARS
+# config_add CONFIG_EFI_STUB
